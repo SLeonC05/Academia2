9f92ba042602db5270c584b3e3da66f85dad501c
SLeonC05 <santiagoleoncarrasco4@gmail.com>
2022-09-08 09:00:20 -0500

    Acutualizacion final

--- resources/views/estudiantes/show.blade.php
+++ resources/views/estudiantes/show.blade.php
@@ -7,27 +7,32 @@
         <div class="m-auto">
                 <h2>Destalles del Estudiante</h2>
                 <br>
-                <p class="card-text"><b>Nombres: </b>{{$estudiantico->nombres}}</p>
-                <p class="card-text"><b>Primer Apellido: </b>{{$estudiantico->primerApellido}}</p>
-                <p class="card-text"><b>Segundo Apellido: </b>{{$estudiantico->segundoApellido}}</p>
-                @foreach ($query2 as $consulta)
-                    <p><b>Pais de nacimiento: </b>{{$consulta->nomPais}}</p>
-                    <p><b>Departamento de nacimiento: </b>{{$consulta->nombreDepart}}</p>
-                    <p><b>Municipio de nacimiento: </b>{{$consulta->nombreMuni}}</p>
-                @endforeach
-                <p class="card-text"><b>Tipo de documento: </b>{{$estudiantico->tipoDoc}}</p>
-                <p class="card-text"><b>Numero de documento: </b>{{$estudiantico->numDoc}}</p><br>
-                <iframe width="400" height="400" src="{{Storage::url($estudiantico->docIdent)}}"></iframe><br><br>
-                <p class="card-text"><b>Fecha de expedición del documento: </b>{{$estudiantico->fechaExp}}</p>
-                @foreach ($query1 as $consulta)
-                    <p><b>Pais de expedicion: </b>{{$consulta->nomPais}}</p>
-                    <p><b>Departamento de expedición: </b>{{$consulta->nombreDepart}}</p>
-                    <p><b>Municipio de expedición: </b>{{$consulta->nombreMuni}}</p>
-                @endforeach
-                <p class="card-text"><b>Genero: </b>{{$estudiantico->genero}}</p>
-                <p class="card-text"><b>Curso: </b>{{$estudiantico->idCurso}}</p>
-                <p class="card-text"><b>Estrato socioeconomico: </b>{{$estudiantico->estrato}}</p>
-
+            <div class="row">
+                <div class="col-6">
+                    <p class="card-text"><b>Nombres: </b>{{$estudiantico->nombres}}</p>
+                    <p class="card-text"><b>Primer Apellido: </b>{{$estudiantico->primerApellido}}</p>
+                    <p class="card-text"><b>Segundo Apellido: </b>{{$estudiantico->segundoApellido}}</p>
+                    @foreach ($query2 as $consulta)
+                        <p><b>Pais de nacimiento: </b>{{$consulta->nomPais}}</p>
+                        <p><b>Departamento de nacimiento: </b>{{$consulta->nombreDepart}}</p>
+                        <p><b>Municipio de nacimiento: </b>{{$consulta->nombreMuni}}</p>
+                    @endforeach
+                    <p class="card-text"><b>Tipo de documento: </b>{{$estudiantico->tipoDoc}}</p>
+                    <p class="card-text"><b>Numero de documento: </b>{{$estudiantico->numDoc}}</p><br>
+                    <iframe width="400" height="400" src="{{Storage::url($estudiantico->docIdent)}}"></iframe><br><br>
+                </div>
+                <div class="col-6">
+                    <p class="card-text"><b>Fecha de expedición del documento: </b>{{$estudiantico->fechaExp}}</p>
+                    @foreach ($query1 as $consulta)
+                        <p><b>Pais de expedicion: </b>{{$consulta->nomPais}}</p>
+                        <p><b>Departamento de expedición: </b>{{$consulta->nombreDepart}}</p>
+                        <p><b>Municipio de expedición: </b>{{$consulta->nombreMuni}}</p>
+                    @endforeach
+                    <p class="card-text"><b>Genero: </b>{{$estudiantico->genero}}</p>
+                    <p class="card-text"><b>Curso: </b>{{$estudiantico->idCurso}}</p>
+                    <p class="card-text"><b>Estrato socioeconomico: </b>{{$estudiantico->estrato}}</p>
+                </div>
+            </div>
         </div>
         <br>
         <a href="/estudiantes/{{$estudiantico->id}}/edit" class="btn btn-success">Editar Estudiante</a>
